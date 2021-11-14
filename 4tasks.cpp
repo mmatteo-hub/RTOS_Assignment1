43cd1074c18649bed987182de00d62722d79c595
teo_ubu <mattemara99@gmail.com>
2021-11-14 17:43:39 +0100

    added mutexes

--- 4tasks.cpp
+++ 4tasks.cpp
@@ -16,6 +16,7 @@
 // mutex semaphores
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex3 = PTHREAD_MUTEX_INITIALIZER;
 
 
 // global variables for tasks
